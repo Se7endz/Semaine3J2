34a2268dc81c07ac3c6604cc3930eeeb53ccaea9
Se7endz <oussama.bendifallahd@gmail.com>
2019-04-17 00:37:23 +0200

    encore4

--- mini_jeu_POO/app2.rb
+++ mini_jeu_POO/app2.rb
@@ -34,7 +34,7 @@ end
 
 
 
-### Debut JEU / LANCEMENT / COMBAT
+### Debut JEU / LANCEMENT
 ##
 #
 
@@ -47,7 +47,7 @@ end
 
 #
 ##
-### Fin JEU / LANCEMENT / COMBAT
+### Fin JEU / LANCEMENT
 
 ### Debut JEU / FIGHT
 ##
@@ -56,32 +56,60 @@ end
 def game_combat
     
     while @hplayer1.life_point >0 && (@player1.life_point > 0 || @player2.life_point >0)
-        puts "\n\n C'est au tour de #{@name} d'attaquer !!\n"
-        @hplayer1.attacks(@player1)
         puts "\n\n Voici l'etat de nos joueurs"
         puts "#{@hplayer1.show_stats}"
         puts "#{@player1.show_stats}"
         puts "#{@player2.show_stats}"
-        break if @hplayer1.life_point <= 0 || (@player1.life_point <= 0 || @player2.life_point <= 0)
-        puts "\n\n C'est au tour de #{@name} d'attaquer !!\n"
-        @hplayer1.attacks(@player1)
-        puts "\n\n Voici l'etat de nos joueurs"
-        puts "#{@hplayer1.show_stats}"
-        puts "#{@player1.show_stats}"
-        puts "#{@player2.show_stats}"
-    end
+        puts "\n\n"
+        puts "what do you want to do ?"
+            puts "a - chercher une meilleure arme"
+            puts "s - chercher à se soigner"
+            puts
+            puts "attaquer un joueur en vue :"
+            puts "1 - #{@player1.name} a #{@player1.show_state} points de vie"
+            puts "2 - #{@player2.name} a #{@player2.show_state} points de vie"
 
+            @action = gets.chomp
     
-    if @player1.life_point <= 0 || @player2.life_point <= 0
-        puts "\n\n GAME OVER !!!\n\n Victoire écrasante de #{@name} !!!!\n Il a EXPLOSE #{@player1.name} et #{@player2.name} !!!!"
+                if @action == "a"
+                    @hplayer1.search_weapon
+                elsif @action == "s"
+                    @hplayer1.search_health_pack
+                elsif @action == "1"
+                    @hplayer1.attack(player_1)
+                elsif @action == "2"
+                    @hplayer1.attack(player_2)
+                else puts "Please select option a, s, 1 or 2"
+                end
 
-    elsif @hplayer1.life_point <= 0
-        puts "\n\n GAME OVER !!!\n\n Victoire écrasante de #{@player1} et #{@player2} !!!!\n Ils ont pulvérisés #{@name}"
-        
-    end
-    puts "\n\n Press Enter pour sortir"
-    gets.chomp
+                puts
+                puts "A l'attaque"
+                ennemies.each { |player| 
+                    if @player.life_point >= 0
+                        @player.attack(@hplayer1)
+                    end
+                }
+
+                round += 1
+                puts
+                puts "Voici l'etat de nos joueurs :"
+                @hplayer1.show_state
+                @player2.show_state
+                @player1.show_state
+                puts 
+            end
+
+        end
+
+puts "========> END OF THE GAME <=========="
+puts
+
+if @hplayer1.life_point > 0
+    puts "You Win !"
+elsif @hplayer1.life_point < 0
+    puts "You Loooose"
 end
+
 
 #
 ##
